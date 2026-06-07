233c3e9d2cb854a7a20ca4632f2f888b5720cf07
Aaron Pham <contact@aarnphm.xyz>
2026-06-06 22:40:02 -0400

    feat: markdown-aware blockquote et al
    Signed-off-by: Aaron Pham <contact@aarnphm.xyz>

--- lua/luasnip-latex-snippets/init.lua
+++ lua/luasnip-latex-snippets/init.lua
@@ -109,6 +109,7 @@ M.setup_markdown = function()
   local to_filter = {}
   for _, str in ipairs({
     "wA",
+    "bwA",
   }) do
     local t = require(("luasnip-latex-snippets.%s"):format(str)).retrieve(not_math)
     vim.list_extend(to_filter, vim.tbl_map(trigger_of_snip, t))
@@ -117,15 +118,79 @@ M.setup_markdown = function()
   local filtered = vim.tbl_filter(function(s)
     return not vim.tbl_contains(to_filter, s.trigger)
   end, autosnippets)
+  vim.list_extend(filtered, require("luasnip-latex-snippets.bwA").retrieve(is_math))
 
   local parse_snippet = ls.extend_decorator.apply(ls.parser.parse_snippet, {
     condition = pipe({ not_math }),
   }) --[[@as function]]
 
+  local function markdown_quote_prefix(line_to_cursor, matched_trigger)
+    local before_trigger = line_to_cursor:sub(1, #line_to_cursor - #matched_trigger)
+    local rest = before_trigger:gsub("^%s*", "")
+    local has_quote_marker = false
+
+    while rest:sub(1, 1) == ">" do
+      has_quote_marker = true
+      rest = rest:sub(2):gsub("^%s*", "")
+    end
+
+    if has_quote_marker and rest == "" then
+      return before_trigger
+    end
+  end
+
+  local function markdown_blockquote(line_to_cursor, matched_trigger)
+    return markdown_quote_prefix(line_to_cursor, matched_trigger) ~= nil
+  end
+
+  local function markdown_not_blockquote(line_to_cursor, matched_trigger)
+    return not markdown_blockquote(line_to_cursor, matched_trigger)
+  end
+
+  local function markdown_blockquote_expand_params(_, line_to_cursor, matched_trigger)
+    local quote_prefix = markdown_quote_prefix(line_to_cursor, matched_trigger)
+    if not quote_prefix then
+      return nil
+    end
+
+    return { env_override = { MARKDOWN_QUOTE_PREFIX = quote_prefix } }
+  end
+
+  local function quote_prefix(snip)
+    return snip.env.MARKDOWN_QUOTE_PREFIX or "> "
+  end
+
+  local function quote_continuation(_, snip)
+    return { "", quote_prefix(snip) }
+  end
+
+  local function quote_close(_, snip)
+    return { "", quote_prefix(snip) .. "$$" }
+  end
+
+  local markdown_parse_snippet = ls.extend_decorator.apply(ls.parser.parse_snippet, {
+    condition = pipe({ not_math, markdown_not_blockquote }),
+  }) --[[@as function]]
+
   -- tex delimiters
   local normal_wA_tex = {
     parse_snippet({ trig = "mk", name = "Math" }, "$${1:${TM_SELECTED_TEXT}}$"),
-    parse_snippet({ trig = "dm", name = "Block Math" }, "$$\n${1:${TM_SELECTED_TEXT}}\n$$"),
+    markdown_parse_snippet(
+      { trig = "dm", name = "Block Math" },
+      "$$\n${1:${TM_SELECTED_TEXT}}\n$$"
+    ),
+    ls.snippet({
+      trig = "dm",
+      name = "Block Math",
+      condition = pipe({ not_math, markdown_blockquote }),
+      resolveExpandParams = markdown_blockquote_expand_params,
+    }, {
+      ls.text_node("$$"),
+      ls.function_node(quote_continuation, { 1 }),
+      ls.insert_node(1),
+      ls.function_node(quote_close, { 1 }),
+      ls.insert_node(0),
+    }),
   }
   vim.list_extend(filtered, normal_wA_tex)
 
